0402d0e8670004be498550ffc705fc5661cffc76
Giansco <gianniscolar@gmail.com>
2017-08-14 18:21:07 -0300

    implemented ex1 and ex3

---
+++ src/main/python/analisis4/Guide1/ex1Gianni.py
@@ -0,0 +1,29 @@
+
+from math import e, inf
+import sys
+def excCiii():
+    i=1
+    while(True):
+        number = e**(-i)
+        print(number)
+        if(number==0.0): sys.exit()
+        print(i)
+        i+=1
+
+#excCiii()
+
+def excA():
+    i=1.0
+    while(i!=inf):
+        print(i)
+        i=i*10
+
+#excA()
+
+def excB():
+    i = 1.0
+    while (i != 0.0):
+        print(i)
+        i = i / 10
+
+excB()
