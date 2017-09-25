a4b26c27ad5bdfd7aa7b7b2205dd36e3c90a9f3f
Ignacio <ignacio.delavega@ing.austral.edu.ar>
2017-09-25 19:06:10 -0300

    Para gianni

--- src/main/python/analisis4/Guide3/Ex9.py
+++ src/main/python/analisis4/Guide3/Ex9.py
@@ -2,10 +2,9 @@ def bisection(function, a, b, presition, n):
     pk = a
     for i in range(0, n):
         pk = (a+b)/2
-        if (b-a)/2 < presition:
-            break
         function_pk = function(pk)
         if function_pk == 0 or (b-a)/2 < presition:
+            print(i)
             return pk
         elif function_pk * function(a) > 0:
             a = pk
@@ -16,5 +15,15 @@ def bisection(function, a, b, presition, n):
     return pk
 
 
+def function(x):
+    return x**3 - x - 1
 
+#print(bisection(function, 1, 2, 10e-3, 1000000))
+
+def funtionEx11(x):
+    return x**4 - 2*x**3 - 4*x**2 + 4*x + 4
+
+print(bisection(function, -2, 0, 10e-3, 10000000))
+print(bisection(function, 0, 2, 10e-3, 10000000))
+print(bisection(function, 1, 2, 10e-3, 10000000))
 
--- src/main/python/analisis4/Guide3/Ex9Edu.py
+++ src/main/python/analisis4/Guide3/Ex9Edu.py
@@ -5,6 +5,7 @@ def bisection(function, a, b, precision, max_iterations):
     for i in range(0, max_iterations):
         pk = (a + b) / 2
         if function(pk) == 0 or (b - a) / 2 < precision:
+            print(i)
             return pk
         if equal_sign(function(pk), function(a)):
             a = pk
@@ -14,3 +15,17 @@ def bisection(function, a, b, precision, max_iterations):
 
 def equal_sign(number1, number2):
     return number1 / math.fabs(number1) == number2 / math.fabs(number2)
+
+
+def function(x):
+    return x**3 - x - 1
+
+#print(bisection(function, 1, 2, 10e-3, 1000000))
+
+
+def funtionEx11(x):
+    return x**4 - 2*x**3 - 4*x**2 + 4*x + 4
+
+print(bisection(function, -2, 0, 10e-8, 10000000))
+print(bisection(function, 0, 2, 10e-8, 10000000))
+print(bisection(function, 1, 2, 10e-8, 10000000))
