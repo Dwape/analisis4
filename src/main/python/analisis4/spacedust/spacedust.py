eb4c4716543254db19283087f37fa72917ef7be2
Ignacio <ignacio.delavega@ing.austral.edu.ar>
2017-10-04 17:12:42 -0300

    File reader

--- src/main/python/analisis4/spacedust/spacedust.py
+++ src/main/python/analisis4/spacedust/spacedust.py
@@ -2,7 +2,7 @@ import csv
 import numpy as np
 
 from src.main.python.analisis4.spacedust.Lagrange import lagrange
-from src.main.python.analisis4.spacedust.grapher import graph3d
+from src.main.python.analisis4.spacedust.grapher import graph3d, graph2d
 
 
 def evaluate_Points(begin, end, function, distance):
@@ -16,25 +16,31 @@ def evaluate_Points(begin, end, function, distance):
     return newLatitud
 
 
-def write_csv2d(begin, end, newLatitud, distance):
-    out = csv.writer(open("interpolated.csv", "w"), delimiter="\n")
+def write_csv2d(begin, end, newLatitud, distance, filename):
+    out = csv.writer(open(filename, "w"), delimiter="\n")
     write = []
     for i in range(0, begin-end, distance):
         write.append(str(i+end) + "," + str(newLatitud[int((begin-end - i) / distance)]))
 
     out.writerow(write)
 
-def write_csv3d(begin, end, newLatitud, newLongitud, distance):
-    out = csv.writer(open("graph3d.csv", "w"), delimiter="\n")
+def write_csv3d(begin, end, newLatitud, newLongitud, distance, filename):
+    out = csv.writer(open(filename, "w"), delimiter="\n")
     write = []
     for i in range(0, begin-end, distance):
-        write.append(str(i+end) + "," + str(newLatitud[int((begin-end - i) / distance)]) + "," + str(newLongitud[int(begin-end- i)/ distance]))
+        write.append(str(i+end) + "," + str(newLatitud[int((begin-end - i) / distance)]) + "," + str(newLongitud[int((begin-end - i )/ distance)]))
 
     out.writerow(write)
 
 def get_column(filename, column):
     results = csv.reader(open(filename), delimiter=",")
-    return np.asarray([result[column] for result in results])
+    array = []
+    for result in results:
+        if len(result) > 0:
+            array.append(result[column])
+    return array
+
+# np.asarray([result[column] for result in results])
 
 
 def getZPoints(size, distance):
@@ -51,44 +57,29 @@ def getZPoints(size, distance):
 #    return  evaluate_Points(int(altura[x1]), int(altura[x2]), function, distance)
 
 
-def graph2d(column):
-    distance = 100
-    yPoints = np.empty([int(10000/distance)])
-    altura = get_column("geo_pos.csv", 0)
-    axisY = get_column("geo_pos.csv", column)
-
-    function1 = lagrange(altura[0:50 + 1], axisY[0:50 + 1])
-    yPoint1 = evaluate_Points(int(altura[0]), int(altura[50]), function1, distance)
-
-    function2 = lagrange(altura[50:94+1], axisY[50:94+1])
-    yPoint2 = evaluate_Points(int(altura[50]), int(altura[94]), function2, distance)
-
-    yPoint3 = evaluate_Points(int(altura[94]), 0, function2, distance)
-
-    yPoints = np.append(yPoint1, yPoint2, axis=0)
-    yPoints = np.append(yPoints, yPoint3, axis=0)
-
-    write_csv2d(10000, 0, yPoints, distance)
-
-def main2():
-    distance = 100
+def fileWriter():
+    distance = 10
     yPoints = np.empty([int(10000 / distance)])
     xPoints = np.empty([int(10000 / distance)])
     altura = get_column("geo_pos.csv", 0)
     latitud = get_column("geo_pos.csv", 1)
     longitud = get_column("geo_pos.csv", 2)
 
-    functionY1 = lagrange(altura[0:50 + 1], latitud[0:50 + 1])
+    functionY1 = lagrange(altura[0:51], latitud[0:51])
     yPoint1 = evaluate_Points(int(altura[0]), int(altura[50]), functionY1, distance)
+    yPoint1 = np.delete(yPoint1, len(yPoint1)-1)
 
-    functionX1 = lagrange(altura[0:50 + 1], longitud[0:50 + 1])
+    functionX1 = lagrange(altura[0:51], longitud[0:51])
     xPoint1 = evaluate_Points(int(altura[0]), int(altura[50]), functionX1, distance)
+    xPoint1 = np.delete(xPoint1, len(xPoint1) - 1)
 
-    functionY2 = lagrange(altura[50:94 + 1], latitud[50:94 + 1])
+    functionY2 = lagrange(altura[50:95], latitud[50:95])
     yPoint2 = evaluate_Points(int(altura[50]), int(altura[94]), functionY2, distance)
+    yPoint2 = np.delete(yPoint2, len(yPoint2) - 1)
 
-    functionX2 = lagrange(altura[50:94 + 1], longitud[50:94 + 1])
+    functionX2 = lagrange(altura[50:95], longitud[50:95])
     xPoint2 = evaluate_Points(int(altura[50]), int(altura[94]), functionX2, distance)
+    xPoint2 = np.delete(xPoint2, len(xPoint2) - 1)
 
     yPoint3 = evaluate_Points(int(altura[94]), 0, functionY2, distance)
     xPoint3 = evaluate_Points(int(altura[94]), 0, functionX2, distance)
@@ -98,16 +89,28 @@ def main2():
     xPoints = np.append(xPoint1, xPoint2, axis=0)
     xPoints = np.append(xPoints, xPoint3, axis=0)
 
-    write_csv3d(10000, 0, yPoints, xPoints, distance)
 
-    graph3d(xPoints, yPoints, getZPoints(10000, distance))
+    write_csv3d(10000, 0, yPoints, xPoints, distance, "interpolated10.csv")
+
+
 
 def createGraph3d():
-    altura = get_column("graph3d.csv", 0)
-    latitud = get_column("graph3d.csv", 1)
-    longitud = get_column("graph3d.csv", 2)
+    altura = get_column("interpolated10.csv", 0)
+    latitud = get_column("interpolated10.csv", 1)
+    longitud = get_column("interpolated10.csv", 2)
 
-main2()
+    graph3d(altura, latitud, longitud)
+
+def createGraph2d(column, name):
+    altura = get_column("interpolated10.csv", 0)
+    axisY = get_column("interpolated10.csv", column)
+
+    graph2d(axisY, altura, name, "Altura")
+
+#fileWriter()
+createGraph2d(1, "Latitud")
+createGraph2d(2, "Longitud")
+createGraph3d()
 
 
 
