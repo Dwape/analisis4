8e8881a6102760e31d89ae461c37130b9de81913
Ignacio <ignacio.delavega@ing.austral.edu.ar>
2017-10-04 15:04:33 -0300

    File reader

--- src/main/python/analisis4/spacedust/spacedust.py
+++ src/main/python/analisis4/spacedust/spacedust.py
@@ -2,7 +2,6 @@ import csv
 import numpy as np
 
 from src.main.python.analisis4.spacedust.Lagrange import lagrange
-from src.main.python.analisis4.spacedust.grapher import graph2d
 
 
 def evaluate_Points(begin, end, function, distance):
@@ -37,20 +36,31 @@ def getXPoints(size, distance):
     return xPoints
 
 
-def calculateLagrange(x1, x2, distance):
-    altura = get_column("geo_pos.csv", 0)
-    latitud = get_column("geo_pos.csv", 1)
-    function = lagrange(altura[x1:x2+1], latitud[x1:x2+1])
-    return  evaluate_Points(int(altura[x1]), int(altura[x2]), function, distance)
+#def calculateLagrange(x1, x2, distance):
+#    altura = get_column("geo_pos.csv", 0)
+#    latitud = get_column("geo_pos.csv", 1)
+#    function = lagrange(altura[x1:x2+1], latitud[x1:x2+1])
+#    return  evaluate_Points(int(altura[x1]), int(altura[x2]), function, distance)
 
 
 def main():
     distance = 10
     yPoints = np.empty([int(10000/distance)])
-    yPoint1 = calculateLagrange(50, 94, distance)
-    yPoints = np.append(yPoints, calculateLagrange(0, 50, distance))
+    altura = get_column("geo_pos.csv", 0)
+    latitud = get_column("geo_pos.csv", 1)
 
-    write_csv(10000, 0, yPoints, 10)
+    function1 = lagrange(altura[0:50 + 1], latitud[0:50 + 1])
+    yPoint1 = evaluate_Points(int(altura[0]), int(altura[50]), function1, distance)
+
+    function2 = lagrange(altura[50:94+1], latitud[50:94+1])
+    yPoint2 = evaluate_Points(int(altura[50]), int(altura[94]), function2, distance)
+
+    yPoint3 = evaluate_Points(int(altura[94]), 0, function2, distance)
+
+    yPoints = np.append(yPoint1, yPoint2, axis=0)
+    yPoints = np.append(yPoints, yPoint3, axis=0)
+
+    write_csv(10000, 0, yPoints, distance)
 
 main()
 
