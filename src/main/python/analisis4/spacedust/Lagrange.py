3d993e9165ec9116103bd52862e81518722b9fcc
Dwape <eduardo.lalor@ing.austral.edu.ar>
2017-10-04 12:08:11 -0300

    Changed lagrange method in Lagrange to make it more readable.

--- src/main/python/analisis4/spacedust/Lagrange.py
+++ src/main/python/analisis4/spacedust/Lagrange.py
@@ -1,28 +1,19 @@
 def lagrange(x_values, y_values):
-    Lx = []
+    """Lagrange method. Returns a function that interpolates the points provided as function
+    parameters. This function can be evaluated in a value of x to get its estimated y value."""
+    lx = []
     for i in range(0, len(x_values)):
-        Li = "(("
+        li = ""
+        ld = ""
         for j in range(0, len(x_values)):
             if j != i:
-                Li = Li + "(x-" + str(x_values[j]) + ")*"
-        Li = Li[:-1]
-        Li += ")"
-        Li += "/("
-        for j in range(0, len(x_values)):
-            if j != i:
-                Li = Li + "(" + str(x_values[i]) + "-" + str(x_values[j]) + ")*"
-        Li = Li[:-1]
-        Li += "))"
-        Lx.append(Li)
+                li += "(x-" + str(x_values[j]) + ")*"
+                ld += "(" + str(x_values[i]) + "-" + str(x_values[j]) + ")*"
+        li = li[:-1]
+        Lc = ld[:-1]
+        lx.append("(" + li + ")/(" + Lc + ")")
     Px = ""
     for i in range(0, len(x_values)):
-        Px += str(y_values[i]) + "*" + Lx[i] + "+"
+        Px += str(y_values[i]) + "*" + lx[i] + "+"
     Px = Px[:-1]
-    print(Px)
     return lambda x: eval(Px, {"x": x})
-
-
-x_values = [1, 2, 4]
-y_values = [1, 2, 4]
-
-print(lagrange(x_values, y_values)(3))
