0ea8b7f991f9a33936b32c806099521ea190d8f6
Dwape <eduardo.lalor@ing.austral.edu.ar>
2017-10-04 18:56:05 -0300

    Grapher should now work correctly.

--- src/main/python/analisis4/spacedust/grapher.py
+++ src/main/python/analisis4/spacedust/grapher.py
@@ -1,57 +1,32 @@
 import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib as mpl
+
 
 def graph2d(x_values, y_values, x_axis, y_axis):
+    """Graphs a curve in 2D, with the values passed as parameters."""
     plt.plot(x_values, y_values)
     plt.xlabel(x_axis)
     plt.ylabel(y_axis)
     plt.show()
 
 
-def graph3di(x_values, y_values, z_values):
-    """Two options:
-    1. z_values don't need to be calculated with the new values of x and y np.meshgrid(x, y). If that
-    is the case, z_values that were calculated previously can be used.
-    2. z_values need to be calculated with the new values of x and y. Then, Lagrange could be applied
-    with np.meshgrid(x, y) and z_values to get new z_values"""
+def graph3d(x_values, y_values, z_values, x_axis, y_axis, z_axis):
+    """Graphs a parametric curve with the values passed as parameters."""
+    mpl.rcParams['legend.fontsize'] = 10
 
     fig = plt.figure()
     ax = fig.gca(projection='3d')
 
-    x_values, y_values = np.meshgrid(x_values, y_values)
+    ax.plot(x_values, y_values, z_values, label='parametric curve')
+    ax.legend()
 
-    # Plot the surface.
-    surf = ax.plot_surface(x_values, y_values, z_values, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-
-    # Customize the z axis.
-    ax.set_zlim(-1.01, 1.01)
-    ax.zaxis.set_major_locator(LinearLocator(10))
-    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-    # Add a color bar which maps values to colors.
-    fig.colorbar(surf, shrink=0.5, aspect=5)
+    ax.set_xlabel(x_axis)
+    ax.set_ylabel(y_axis)
+    ax.set_zlabel(z_axis)
 
     plt.show()
 
 
-def graph3d(x_values, y_values, z_values):
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    # Make data
-    x = np.outer(x_values, y_values)
-    y = np.outer(y_values, x_values)
-    z = z_values
-
-    # Plot the surface
-    ax.plot_surface(x, y, z, color='b')
-
-    ax.zaxis.set_major_locator(LinearLocator(10))
-    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-    plt.show()
 
