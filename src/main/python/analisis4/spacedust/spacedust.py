71a9c0f99163e4ae64c1dc4e202cb42b72d76674
Ignacio <ignacio.delavega@ing.austral.edu.ar>
2017-10-04 13:03:51 -0300

    File reader

--- src/main/python/analisis4/spacedust/spacedust.py
+++ src/main/python/analisis4/spacedust/spacedust.py
@@ -1,12 +1,12 @@
 import csv
+import numpy as np
 
 from src.main.python.analisis4.spacedust.Lagrange import lagrange
 
 
-def write_csv(begin, end):
-
+def write_csv(begin, end, function):
     newLatitud = []
-    for i in range(begin, end, -10):
+    for i in range(begin, end-1, -10):
         newLatitud.append(function(i))
         print(i)
         print(function(i))
@@ -23,15 +23,14 @@ def get_column(filename, column):
     return [result[column] for result in results]
 
 
+def calculateLagrange(x1, x2):
+    altura = get_column("geo_pos.csv", 0)
+    latitud = get_column("geo_pos.csv", 1)
+    function = lagrange(altura[x1:x2], latitud[x1:x2])
+    write_csv(int(altura[x1]),  int(altura[x2]), function)
 
 
-
-altura = get_column("geo_pos.csv", 0)
-latitud = get_column("geo_pos.csv", 1)
-function = lagrange(altura[0:100], latitud[0:100])
-write_csv(10000,9899)
-
-
+calculateLagrange(0, 50)
 
 
 
