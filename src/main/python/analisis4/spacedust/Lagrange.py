bab0027c95887189dbf8f06c4cc6c805214e2985
Dwape <eduardo.lalor@ing.austral.edu.ar>
2017-10-06 10:43:02 -0300

    Further improvements to lagrange method in Lagrange. It should now be more efficient.

--- src/main/python/analisis4/spacedust/Lagrange.py
+++ src/main/python/analisis4/spacedust/Lagrange.py
@@ -1,19 +1,16 @@
 def lagrange(x_values, y_values):
     """Lagrange method. Returns a function that interpolates the points provided as function
     parameters. This function can be evaluated in a value of x to get its estimated y value."""
-    lx = []
+    px = ""
     for i in range(0, len(x_values)):
-        li = ""
-        ld = ""
+        li, ld = "", ""
         for j in range(0, len(x_values)):
             if j != i:
                 li += "(x-" + str(x_values[j]) + ")*"
                 ld += "(" + str(x_values[i]) + "-" + str(x_values[j]) + ")*"
         li = li[:-1]
         ld = ld[:-1]
-        lx.append("(" + li + ")/(" + ld + ")")
-    px = ""
-    for i in range(0, len(x_values)):
-        px += str(y_values[i]) + "*" + lx[i] + "+"
+        px += str(y_values[i]) + "*" + "(" + li + ")/(" + ld + ")" + "+"
     px = px[:-1]
+    print(px)
     return lambda x: eval(px, {"x": x})
