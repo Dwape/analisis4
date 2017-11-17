49bd0cb8bd7404a887acefdb2ec0637d1c66403a
Dwape <eduardo.lalor@ing.austral.edu.ar>
2017-11-17 12:41:42 -0300

    Runge-Kutta now correctly for Newton's law of cooling.

--- src/main/python/analisis4/ElHorno/RungeKutta.py
+++ src/main/python/analisis4/ElHorno/RungeKutta.py
@@ -1,18 +1,21 @@
-def function(x, y):
-    return (-0.25)*(x-y)
-
-
-def runge_kutta(function, h, x0, y0, x):
-    """Calculates the value of y(x0+h) using the Runge-Kutta method."""
+def runge_kutta(function, h, x0, y0, x, tm):
+    """Calculates the value of y(x0+h) using the Runge-Kutta method
+    for Newton's law of cooling, where tm is the temperature of the environment."""
     while x0 < x:
-        f1 = h * function(x0, y0)
-        f2 = h * function(x0 + 1 / 2 * h, y0 + 1 / 2 * f1)
-        f3 = h * function(x0 + 1 / 2 * h, y0 + 1 / 2 * f2)
-        f4 = h * function(x0 + h, y0 + f3)
+        f1 = h * function(y0, tm)
+        f2 = h * function(y0 + 1 / 2 * f1, tm)
+        f3 = h * function(y0 + 1 / 2 * f2, tm)
+        f4 = h * function(y0 + f3, tm)
         y0 += 1 / 6 * (f1 + 2 * f2 + 2 * f3 + f4)
         x0 += h
     return y0
 
+
+def temperature_function(y, tm):
+    return -0.25 * (y - tm)
+
+
+#print(runge_kutta(temperature_function, 0.1, 0, 150, 5, 62.25))
 
 '''
 def runge_kutta(function, h, x0, y0):
@@ -23,6 +26,20 @@ def runge_kutta(function, h, x0, y0):
     f4 = h * function(x0 + h, y0 + f3)
 
     return y0 + 1 / 6 * (f1 + 2 * f2 + 2 * f3 + f4)
+
+def runge_kutta2(function, h, x0, y0, x):
+    """Calculates the value of y(x0+h) using the Runge-Kutta method."""
+    while x0 < x:
+        f1 = h * function(x0, y0)
+        f2 = h * function(x0 + 1 / 2 * h, y0 + 1 / 2 * f1)
+        f3 = h * function(x0 + 1 / 2 * h, y0 + 1 / 2 * f2)
+        f4 = h * function(x0 + h, y0 + f3)
+        y0 += 1 / 6 * (f1 + 2 * f2 + 2 * f3 + f4)
+        x0 += h
+    return y0
+
+def function1(x, y, tm):
+    return -0.25 * (y - tm)
 '''
 
 
